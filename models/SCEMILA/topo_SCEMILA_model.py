added loggiong functionality to deep topo added multi threading, added calculation timeout functionality; fixed no mil loss bug; added thread system stats for pc and hpc systems

models/SCEMILA/topo_SCEMILA_model.py
@@ -110,10 +110,8 @@ class TopoAMiL(nn.Module):
         x= self.ensure_square_tensor(input_distance_matrix)
         y= self.ensure_square_tensor(latent_distance_matrix)
         assert x.shape == y.shape
-        topo_loss, topo_loss_components = self.topo_sig(x, y)
-        
-        topo_loss = topo_loss / float(latent_distance_matrix.shape[0])
-        return {"topo_loss":topo_loss,"topo_loss_components": topo_loss_components}
+        topo_loss_output = self.topo_sig(x, y)
+        return {"topo_loss":topo_loss_output[0],"topo_step_log": topo_loss_output[1]}
 
     def ensure_square_tensor(self,tensor):
         # Check if tensor is 1 x n x n

models/topology_models/custom_topo_tools/topo_encoder.py
@@ -17,9 +17,6 @@ class ConnectivityEncoderCalculator:
         self.distance_of_persistence_pairs = None
         # self.sanity_checker = []
         #self.topo_progression_stats= [np.unique(zero_scale_topo, return_counts=True)]
-
-    def estimate_distance_from_scale(self,scale):
-        return scale * self.scales[-1]
     def get_component_birthed_at_index(self,index):
         state = self.topo_scale_evolution[index + 1]
         pers_pair = self.persistence_pairs[index]
@@ -40,7 +37,7 @@ class ConnectivityEncoderCalculator:
                 edge,distance = self.get_shortest_distance_between_2_sets_ignoring_all_other_points(set_dict[index_keys[i]],set_dict[index_keys[j]])
                 dist_mat[i,j] = distance
                 dist_mat[j,i] = distance
-                set_to_edge_mapping[(i,j)] = edge
+                set_to_edge_mapping[(i,j)] = (int(edge[0]),int(edge[1]))
         smaller_homology_problem = ConnectivityEncoderCalculator(distance_mat=dist_mat)
         smaller_homology_problem.calculate_connectivity()
         pers_pairs = smaller_homology_problem.persistence_pairs

models/topology_models/topo_tools/moor_topo_reg.py
@@ -136,5 +136,5 @@ class TopologicalSignatureDistance(nn.Module):
 
             distance = distance1_2 + distance2_1
 
-        return distance, distance_components
+        return distance/float(distances1.shape[0]), distance_components
     