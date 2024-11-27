added new topo reg testing configs, added hierarchical hydra configuration, added validation topo option, fixed 0 worker bug, added 16 precision to help with memory problems, added lazy imports to all factory design scripts

models/encoder_models/encoder_model.py
@@ -3,54 +3,93 @@ import torchvision.models as models
 import torch
 
 input_types = ("images","gray_images","fnl34","dino_bloom_small")
+renset_types = [18,34,50]
+
+def get_resnet_version(name):
+    if name == "images" or name == "images18":
+        return 18
+    if name == "images34":
+        return 34
+    if name == "images50":
+        return 50
 
 def get_loss_function(function_name = "cross_entropy"):
     pass
-
 def get_input_encoder(model,input_type = "images",pretrained = False, dropout = None,load_encoder_path = None):
     encoder_output_dim = 500
-    
-    if input_type == "images": #this and the one after should be the same, since Hehr supposedly used the same network on the same 3x144x144 input 
+    if "images" in input_type and not ("gray" in input_type): #this and the one after should be the same, since Hehr supposedly used the same network on the same 3x144x144 input 
         FT_DIM_IN = 512
-        if pretrained:
-            resnet18 = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
-        else:
-            resnet18 = models.resnet18()
+        RESNET = get_resnet_version(input_type)
+        if RESNET == 18:
+            FT_DIM_IN = 512
+            if pretrained:
+                res18 = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
+            else:
+                res18 = models.resnet18()
+            res18 = list(res18.children())[:- 2]#-2
+            if dropout is not None and dropout != 0:
+                res18 = [res18[0],res18[1],res18[2],res18[3],res18[4],nn.Dropout(dropout),res18[5],nn.Dropout(dropout),res18[6],nn.Dropout(dropout),res18[7],nn.Dropout(dropout)]#
+            resnet = res18
+        elif RESNET == 34:
+            FT_DIM_IN = 512
+            if pretrained:
+                resnet34 = models.resnet34(weights=models.ResNet34_Weights.DEFAULT)
+            else:
+                resnet34 = models.resnet34()
 
-        res18 = list(resnet18.children())[:-2]
-        if dropout is not None and dropout != 0:
-            res18 = [res18[0],res18[1],res18[2],res18[3],res18[4],nn.Dropout(dropout),res18[5],nn.Dropout(dropout),res18[6],nn.Dropout(dropout),res18[7],nn.Dropout(dropout)]
- 
-        encoder = nn.Sequential(*res18,
-            nn.Conv2d(FT_DIM_IN, int(FT_DIM_IN * 1.5), kernel_size=2),
+            res34 = list(resnet34.children())[:-2]
+
+            # Add dropout layers, if specified
+            if dropout is not None and dropout != 0:
+                res34 = [res34[0], res34[1], res34[2], res34[3], res34[4],
+                        nn.Dropout(dropout), res34[5], nn.Dropout(dropout), res34[6], nn.Dropout(dropout), res34[7], nn.Dropout(dropout)]
+            resnet = res34
+        elif RESNET == 50:
+            FT_DIM_IN = 2048
+            if pretrained:
+                resnet50 = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
+            else:
+                resnet50 = models.resnet50()
+            res50 = list(resnet50.children())[:-2]
+
+            # Add dropout layers, if specified
+            if dropout is not None and dropout != 0:
+                res50 = [res50[0], res50[1], res50[2], res50[3], res50[4],
+                        nn.Dropout(dropout), res50[5], nn.Dropout(dropout), res50[6], nn.Dropout(dropout), res50[7], nn.Dropout(dropout)]
+            resnet = res50
+            
+
+
+        encoder = nn.Sequential(*resnet,
+            nn.Conv2d(FT_DIM_IN, int(FT_DIM_IN * 0.5), kernel_size=3),
             nn.ReLU(),
-            nn.Conv2d(int(FT_DIM_IN * 1.5), int(FT_DIM_IN * 2), kernel_size=2),
+            nn.Conv2d(int(FT_DIM_IN * 0.5), int(FT_DIM_IN * 0.25), kernel_size=3),
             nn.ReLU(),
             nn.AdaptiveMaxPool2d(output_size=(1, 1)),
             nn.Flatten(),
-            nn.Linear(int(FT_DIM_IN * 2), encoder_output_dim),
+            nn.Linear(int(FT_DIM_IN * 0.25), encoder_output_dim),
             nn.ReLU(),
         )
     elif input_type == "gray_images": #this and the one after should be the same, since Hehr supposedly used the same network on the same 3x144x144 input 
         FT_DIM_IN = 512
         if pretrained:
-            resnet18 = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
+            resnet = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
         else:
-            resnet18 = models.resnet18()
-        resnet18.conv1 = nn.Conv2d(
+            resnet = models.resnet18()
+        resnet.conv1 = nn.Conv2d(
         in_channels=1, 
-        out_channels=resnet18.conv1.out_channels, 
-        kernel_size=resnet18.conv1.kernel_size, 
-        stride=resnet18.conv1.stride, 
-        padding=resnet18.conv1.padding, 
-        bias=resnet18.conv1.bias
+        out_channels=resnet.conv1.out_channels, 
+        kernel_size=resnet.conv1.kernel_size, 
+        stride=resnet.conv1.stride, 
+        padding=resnet.conv1.padding, 
+        bias=resnet.conv1.bias
         )
 
         # If using pretrained weights, adjust them to work with the new layer
         # The weights from the original 3 channels can be averaged to initialize the 1 channel.
         with torch.no_grad():
-            resnet18.conv1.weight[:, 0, :, :] = resnet18.conv1.weight.mean(dim=1)
-        res18 = list(resnet18.children())[:-2]
+            resnet.conv1.weight[:, 0, :, :] = resnet.conv1.weight.mean(dim=1)
+        res18 = list(resnet.children())[:-2]
         if dropout is not None and dropout != 0:
             res18 = [res18[0],res18[1],res18[2],res18[3],res18[4],nn.Dropout(dropout),res18[5],nn.Dropout(dropout),res18[6],nn.Dropout(dropout),res18[7],nn.Dropout(dropout)]
  

models/model_factory.py
@@ -1,35 +1,35 @@
-
-from models.SCEMILA.topo_SCEMILA_model import TopoAMiL
-from models.salome_models.mil import CV_MIL
-from models.salome_models.topo_reg_mil import CV_TopoRegMIL
-from models.SCEMILA.SCEMILA_model import AMiL
-from torch.optim import SGD
-from models.salome_models.optimizer import Adam,SGD
-from models.salome_models.scheduler import ConstantScheduler
-
+import importlib
 
 MODULES = {
-    "CV_MIL": CV_MIL,
-    "CV_TopoRegMIL": CV_TopoRegMIL,
-    "Adam": Adam,
-    "ConstantScheduler": ConstantScheduler,
-    "SCEMILA":AMiL,
-    "TopoSCEMILA":TopoAMiL,
-    "SGD" : SGD
+    "CV_MIL": "models.salome_models.mil.CV_MIL",
+    "CV_TopoRegMIL": "models.salome_models.topo_reg_mil.CV_TopoRegMIL",
+    "Adam": "models.salome_models.optimizer.Adam",
+    "ConstantScheduler": "models.salome_models.scheduler.ConstantScheduler",
+    "SCEMILA": "models.SCEMILA.SCEMILA_model.AMiL",
+    "TopoSCEMILA": "models.SCEMILA.topo_SCEMILA_model.TopoAMiL",
+    "SGD": "torch.optim.SGD",
 }
 
 NAME_KEY: str = "name"
 CONFIG_KEY: str = "config"
 
 
+def lazy_import(path: str):
+    """Lazy imports a module and retrieves the required class or function."""
+    module_path, class_name = path.rsplit(".", 1)  # Split into module and class name
+    module = importlib.import_module(module_path)  # Dynamically import the module
+    return getattr(module, class_name)  # Retrieve the class or function
+
+
 def get_module(name: str, config):
     """Recursively deserializes objects registered in MODULES."""
     if name not in MODULES:
         raise KeyError(
-            f"{name} not found in registered modules. Available are {MODULES.keys()}."
+            f"{name} not found in registered modules. Available are {list(MODULES.keys())}."
         )
-    cls = MODULES[name]
+    cls_path = MODULES[name]
+    cls = lazy_import(cls_path)  # Dynamically load the class
     for key, value in config.items():
-        if(isinstance(value, dict)) and NAME_KEY in value and CONFIG_KEY in value:
-                config[key] = get_module(value[NAME_KEY], value[CONFIG_KEY])
+        if isinstance(value, dict) and NAME_KEY in value and CONFIG_KEY in value:
+            config[key] = get_module(value[NAME_KEY], value[CONFIG_KEY])
     return cls(**config)

models/topology_models/custom_topo_tools/connectivity_topo_regularizer.py
@@ -87,7 +87,7 @@ class TopologicalZeroOrderLoss(nn.Module):
         self.loss_fnc = self.get_torch_p_order_function()
         self.topo_feature_loss = self.get_topo_feature_approach(method)
         self.balance_push_pull = balance_push_pull
-        
+        self.calculate_all_losses = False
         self.timeout = timeout
         self.multithreading= multithreading
         if self.multithreading and method == "deep":
@@ -115,7 +115,7 @@ class TopologicalZeroOrderLoss(nn.Module):
 
    
     def deep_scale_distribution_matching_loss_of_s1_on_s2(self, topo_encoding_space_1: ConnectivityEncoderCalculator, distances1, topo_encoding_space_2: ConnectivityEncoderCalculator, distances2):
-        if distances2.requires_grad:            
+        if distances2.requires_grad or self.calculate_all_losses:            
             self.stop_event.clear()
             nb_of_persistent_pairs = len(topo_encoding_space_1.persistence_pairs)
             shuffled_indices_of_topo_features= list(range(nb_of_persistent_pairs))